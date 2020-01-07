6a1e3f85a44162a49c5c99044039f44a1f3bfce6
talejo <tzalejo@gmail.com>
2020-01-07 20:35:08 -0300

    se actualiza los seed para que las comisiones tenga inicio y fin de este año

--- database/seeds/ComisionSeeder.php
+++ database/seeds/ComisionSeeder.php
@@ -16,36 +16,36 @@ class ComisionSeeder extends Seeder
         Comision::create([
             'comisionNombre'=>'Comision 1',
             'comisionHorario'=>'LUN,MAR,MIER',
-            'comisionFI'=> '2019-1-1', //date('Y-m-d','now') ,
-            'comisionFF'=>'2020-1-1',  //dateTimeThisYear('now', null) ,
+            'comisionFI'=> '2020-1-1', //date('Y-m-d','now') ,
+            'comisionFF'=>'2021-1-1',  //dateTimeThisYear('now', null) ,
             'cursoId'=> 1 ,
         ]);
         Comision::create([
             'comisionNombre'=>'Comision 2',
             'comisionHorario'=>'LUN,MIER,VIE',
-            'comisionFI'=> '2019-1-1', //date('Y-m-d','now') ,
-            'comisionFF'=>'2020-1-1',  //dateTimeThisYear('now', null) ,
+            'comisionFI'=> '2020-1-1', //date('Y-m-d','now') ,
+            'comisionFF'=>'2021-1-1',  //dateTimeThisYear('now', null) ,
             'cursoId'=> 1 ,
         ]);
         Comision::create([
             'comisionNombre'=>'Comision 3',
             'comisionHorario'=>'LUN,MIER,VIE',
-            'comisionFI'=> '2019-1-1', //date('Y-m-d','now') ,
-            'comisionFF'=>'2020-1-1',  //dateTimeThisYear('now', null) ,
+            'comisionFI'=> '2020-1-1', //date('Y-m-d','now') ,
+            'comisionFF'=>'2021-1-1',  //dateTimeThisYear('now', null) ,
             'cursoId'=> 1 ,
         ]);
         Comision::create([
             'comisionNombre'=>'Comision 4',
             'comisionHorario'=>'LUN,MIER,VIE',
-            'comisionFI'=> '2019-1-1', //date('Y-m-d','now') ,
-            'comisionFF'=>'2020-1-1',  //dateTimeThisYear('now', null) ,
+            'comisionFI'=> '2020-1-1', //date('Y-m-d','now') ,
+            'comisionFF'=>'2021-1-1',  //dateTimeThisYear('now', null) ,
             'cursoId'=> 1 ,
         ]);
         Comision::create([
             'comisionNombre'=>'Comision 5',
             'comisionHorario'=>'VIE',
-            'comisionFI'=> '2019-1-4', //date('Y-m-d','now') ,
-            'comisionFF'=>'2020-1-4',  //dateTimeThisYear('now', null) ,
+            'comisionFI'=> '2020-1-4', //date('Y-m-d','now') ,
+            'comisionFF'=>'2021-1-4',  //dateTimeThisYear('now', null) ,
             'cursoId'=> 2 ,
         ]);
     }
