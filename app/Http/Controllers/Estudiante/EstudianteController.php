f29d8c369934810b3c2d777d4e2878dbfbe49c17
alejandro <tzalejo@gmail.com>
2020-09-21 19:37:39 -0300

    mod: refactorizo store de estudiante

--- app/Http/Controllers/Estudiante/EstudianteController.php
+++ app/Http/Controllers/Estudiante/EstudianteController.php
@@ -57,45 +57,45 @@ class EstudianteController extends ApiController
             'estudianteNacimiento'  => $request->estudianteNacimiento,
             'estudianteFoto'        => $request->estudianteFoto,
         ]);
-
+        return $this->showOne($estudianteNuevo);
         // return ($request->get('pagoInscripcion') && $request->get('pagoCuota'));
         # pregunto por las casillas, pero siempre va al else por como tengo el formulario(que puede que se tenga q cambiar)
-        if ($request->get('pagoInscripcion') || $request->get('pagoCuota')) {
-            # Recordar: regular(RE), no regular(NR) o egresado(EG)
-            $matricula =  Matricula::create([
-                'matriculaSituacion'=>'RE',
-                'estudianteId'      =>$estudianteNuevo->estudianteId ,
-                'comisionId'        =>$request->get('comisionId') ,
-            ]);
-            # Si alguna casilla(inscripcion o cuota) esta tildada.
-            # generar las cuotas de la matricula..
-            $nuevafecha = $matricula->comision->comisionFI;
-            Cuota::create([
-                'cuotaConcepto'     => 'Inscripcion - '.$matricula->comision->curso->cursoNombre,
-                'cuotaMonto'        => $matricula->comision->curso->cursoInscripcion,
-                'cuotaFVencimiento' => $nuevafecha,
-                'cuotaBonificacion' => 0,
-                'matriculaId'       => $matricula->matriculaId,
-                ]);
-            for ($i=1; $i <= $matricula->comision->curso->cursoNroCuota ; $i++) {
-                # code...
-                Cuota::create([
-                    'cuotaConcepto'     => 'Cuota '.$i.' - '.$matricula->comision->curso->cursoNombre,
-                    'cuotaMonto'        => $matricula->comision->curso->cursoCostoMes,
-                    'cuotaFVencimiento' => $nuevafecha,
-                    'cuotaBonificacion' => 0,
-                    'matriculaId'       => $matricula->matriculaId,
-                    ]);
-                $nuevafecha = strtotime('+'.$i.' month', strtotime($matricula->comision->comisionFI)) ;
-                $nuevafecha = date('Y-m-j', $nuevafecha);
-            }
-            # return redirect()->route('alumnos.cuotas', $matricula);
-            return $this->showOne($matricula);
-        } else {
-            # es porque ninguna de las dos casilla esta tildada..
-        # return view('home')->with('comisiones', Comision::all());
-        return $this->successResponse(null, 200); # devuelvo '0' para saber q solo se inscribio y no se matriculo(pago inscripcion y/o cuota)
-        }
+        // if ($request->get('pagoInscripcion') || $request->get('pagoCuota')) {
+        //     # Recordar: regular(RE), no regular(NR) o egresado(EG)
+        //     $matricula =  Matricula::create([
+        //         'matriculaSituacion'=>'RE',
+        //         'estudianteId'      =>$estudianteNuevo->estudianteId ,
+        //         'comisionId'        =>$request->get('comisionId') ,
+        //     ]);
+        //     # Si alguna casilla(inscripcion o cuota) esta tildada.
+        //     # generar las cuotas de la matricula..
+        //     $nuevafecha = $matricula->comision->comisionFI;
+        //     Cuota::create([
+        //         'cuotaConcepto'     => 'Inscripcion - '.$matricula->comision->curso->cursoNombre,
+        //         'cuotaMonto'        => $matricula->comision->curso->cursoInscripcion,
+        //         'cuotaFVencimiento' => $nuevafecha,
+        //         'cuotaBonificacion' => 0,
+        //         'matriculaId'       => $matricula->matriculaId,
+        //         ]);
+        //     for ($i=1; $i <= $matricula->comision->curso->cursoNroCuota ; $i++) {
+        //         # code...
+        //         Cuota::create([
+        //             'cuotaConcepto'     => 'Cuota '.$i.' - '.$matricula->comision->curso->cursoNombre,
+        //             'cuotaMonto'        => $matricula->comision->curso->cursoCostoMes,
+        //             'cuotaFVencimiento' => $nuevafecha,
+        //             'cuotaBonificacion' => 0,
+        //             'matriculaId'       => $matricula->matriculaId,
+        //             ]);
+        //         $nuevafecha = strtotime('+'.$i.' month', strtotime($matricula->comision->comisionFI)) ;
+        //         $nuevafecha = date('Y-m-j', $nuevafecha);
+        //     }
+        //     # return redirect()->route('alumnos.cuotas', $matricula);
+        //     return $this->showOne($matricula);
+        // } else {
+        //     # es porque ninguna de las dos casilla esta tildada..
+        // # return view('home')->with('comisiones', Comision::all());
+        // return $this->successResponse(null, 200); # devuelvo '0' para saber q solo se inscribio y no se matriculo(pago inscripcion y/o cuota)
+        // }
     }
     /**
      * Este api lo usabaa para filtrado por DNI y/o por apellido.
