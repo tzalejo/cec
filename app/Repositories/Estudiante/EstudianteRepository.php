557122c1bbda45598cb75a5592dad0367dab4bc5
alejandro <tzalejo@gmail.com>
2020-10-06 19:25:30 -0300

    mod: no usaba la libreria

--- app/Repositories/Estudiante/EstudianteRepository.php
+++ app/Repositories/Estudiante/EstudianteRepository.php
@@ -4,14 +4,16 @@ namespace App\Repositories\Estudiante;
 
 use App\Estudiante;
 use App\Repositories\BaseRepository;
-
+use Carbon\Carbon;
 class EstudianteRepository extends BaseRepository {
 
-    public function getModel(){
+    public function getModel()
+    {
         return new Estudiante();
     }
-    
-    public function getEstudianteDniApellido($dni = null, $apellido = null){
+
+    public function getEstudianteDniApellido($dni = null, $apellido = null)
+    {
         return $this->getModel()
                 ->with('matriculas.comision.curso')
                 ->Apellido($apellido)   # utilizamos scope
