6aa3d7a0e872c32e143da8af379d690658b44ec0
alejandro <tzalejo@gmail.com>
2020-12-12 02:26:11 -0300

    mod: envio el rol del usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,14 +19,22 @@ class AuthController extends Controller
     public function signup(SignupUserRequest $request)
     {
         $user = User::create([
-            'userNombre'    => $request->userNombre,
-            'email'         => $request->email,
-            'password'      => Hash::make($request->password),
-            'userImagen'    => 'secretaria.png',
-            'roleId'        => 2,
+            'userNombre' => $request->userNombre,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'userImagen' => 'secretaria.png',
+            'roleId' => 2,
         ]);
 
-        return $this->successResponse(['message' => 'Usuario '.$user->userNombre.' creado satisfactoriamente'], Response::HTTP_CREATED);
+        return $this->successResponse(
+            [
+                'message' =>
+                    'Usuario ' .
+                    $user->userNombre .
+                    ' creado satisfactoriamente',
+            ],
+            Response::HTTP_CREATED
+        );
     }
     public function login(LoginUserRequest $request)
     {
@@ -34,38 +42,51 @@ class AuthController extends Controller
 
         # El metrod Auth devuelve true si la autenticacion fue exitosa
         if (!Auth::attempt($credenciales)) {
-            return $this->errorResponse('No autorizado, verifique sus datos por favor.', Response::HTTP_UNAUTHORIZED);
+            return $this->errorResponse(
+                'No autorizado, verifique sus datos por favor.',
+                Response::HTTP_UNAUTHORIZED
+            );
         }
         # return response()->json($credenciales);
         # $user = User::find(1);
         # return response()->json($user->createToken('Personal Access Token'));
         $user = $request->user();
         $tokenResult = $user->createToken('Pernsal Access Token');
-        $token =  $tokenResult->token;
+        $token = $tokenResult->token;
 
         if ($request->remember_me) {
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
 
         $token->save();
-        return $this->successResponse([
-            'userNombre'    => $user->userNombre,
-            'email'         => $user->email,
-            'userImagen'    => $user->userImagen,
-            'roleId'        => $user->roleId,
-            'token'         => $tokenResult->accessToken,
-            'token_type'    => 'Bearer',
-            'expires_at'    => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString(),
-        ], Response::HTTP_OK);
+        return $this->successResponse(
+            [
+                'userNombre' => $user->userNombre,
+                'email' => $user->email,
+                'userImagen' => $user->userImagen,
+                'roleId' => $user->roleId,
+                'roleNombre' => $user->role->roleDescripcion,
+                'token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString(),
+            ],
+            Response::HTTP_OK
+        );
     }
-
 
     public function logout(Request $request)
     {
-        $request->user()->token()->revoke();
-        return $this->successResponse(['message'=>'Salió exitosamente'], Response::HTTP_OK);
+        $request
+            ->user()
+            ->token()
+            ->revoke();
+        return $this->successResponse(
+            ['message' => 'Salió exitosamente'],
+            Response::HTTP_OK
+        );
     }
-
 
     public function delete(Request $request)
     {
