866de9e7ea6eae76008c5d2670e8a089ea12ca98
alejandro <tzalejo@gmail.com>
2020-12-30 17:04:05 -0300

    mod: validar que un modelo fue borrado

--- tests/Feature/ComisionModuleTest.php
+++ tests/Feature/ComisionModuleTest.php
@@ -15,7 +15,6 @@ use Faker\Generator as Faker;
 class ComisionModuleTest extends TestCase
 {
     use RefreshDatabase; # para actualiza la base con la base_test(realiza las migraciones)
-
     public function test_obtener_las_comisiones()
     {
         $this->withoutMiddleware();
@@ -31,21 +30,21 @@ class ComisionModuleTest extends TestCase
     {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->create()->toArray();
+        $comision = factory(Comision::class)
+            ->create()
+            ->toArray();
 
-        $this->get(sprintf('api/comisiones/%s',$comision['comisionId']))
+        $this->get(sprintf('api/comisiones/%s', $comision['comisionId']))
             ->assertOk()
             ->assertStatus(Response::HTTP_OK)
-            ->assertJson(
-                [
-                    'comisionNombre' => $comision['comisionNombre'],
-                    'comisionHorario' => $comision['comisionHorario'],
-                    'comisionFI' => $comision['comisionFI'],
-                    'comisionFF' => $comision['comisionFF'],
-                    'cursoId' => $comision['cursoId'],
-                    'comisionId' => $comision['comisionId'],
-                ]
-            );
+            ->assertJson([
+                'comisionNombre' => $comision['comisionNombre'],
+                'comisionHorario' => $comision['comisionHorario'],
+                'comisionFI' => $comision['comisionFI'],
+                'comisionFF' => $comision['comisionFF'],
+                'cursoId' => $comision['cursoId'],
+                'comisionId' => $comision['comisionId'],
+            ]);
     }
 
     public function test_obtener_todas_las_comisiones_inactivas_con_fecha_de_inicio_desde_hasta()
@@ -53,33 +52,37 @@ class ComisionModuleTest extends TestCase
         $this->withoutMiddleware();
         factory(Curso::class, 3)->create();
         factory(Comision::class)->create([
-                'comisionNombre' => 'Comision 41',
-                'comisionHorario' => 'Wednesday 02:33:54',
-                'comisionFI' => '2018-10-15',
-                'comisionFF' => '2019-07-15',
-                'cursoId' => 1,
-                'comisionId' => 1,
+            'comisionNombre' => 'Comision 41',
+            'comisionHorario' => 'Wednesday 02:33:54',
+            'comisionFI' => '2018-10-15',
+            'comisionFF' => '2019-07-15',
+            'cursoId' => 1,
+            'comisionId' => 1,
         ]);
         factory(Comision::class)->create([
-                'comisionNombre' => 'Comision 42',
-                'comisionHorario' => 'Wednesday 02:33:54',
-                'comisionFI' => '2017-10-15',
-                'comisionFF' => '2018-07-15',
-                'cursoId' => 3,
-                'comisionId' => 2,
+            'comisionNombre' => 'Comision 42',
+            'comisionHorario' => 'Wednesday 02:33:54',
+            'comisionFI' => '2017-10-15',
+            'comisionFF' => '2018-07-15',
+            'cursoId' => 3,
+            'comisionId' => 2,
         ]);
         factory(Comision::class)->create([
-                'comisionNombre' => 'Comision 43',
-                'comisionHorario' => 'Wednesday 02:33:54',
-                'comisionFI' => '2020-10-15',
-                'comisionFF' => '2021-07-15',
-                'cursoId' => 3,
-                'comisionId' => 3,
+            'comisionNombre' => 'Comision 43',
+            'comisionHorario' => 'Wednesday 02:33:54',
+            'comisionFI' => '2020-10-15',
+            'comisionFF' => '2021-07-15',
+            'cursoId' => 3,
+            'comisionId' => 3,
         ]);
 
-        $this->get(sprintf('api/comisiones/%s/%s/inactivas',
-            Carbon::parse('2016-01-15')->format('Y-m-d'),
-            Carbon::parse( '2018-07-15')->format('Y-m-d')))
+        $this->get(
+            sprintf(
+                'api/comisiones/%s/%s/inactivas',
+                Carbon::parse('2016-01-15')->format('Y-m-d'),
+                Carbon::parse('2018-07-15')->format('Y-m-d')
+            )
+        )
             ->assertOk()
             ->assertJsonCount(1)
             ->assertStatus(Response::HTTP_OK);
@@ -90,8 +93,9 @@ class ComisionModuleTest extends TestCase
         $this->withoutMiddleware();
         factory(Curso::class)->create();
         factory(Comision::class)->create();
-        $this->get('api/comisiones/fecha_invalida/fecha_invalida')
-            ->assertStatus(Response::HTTP_NOT_FOUND);
+        $this->get(
+            'api/comisiones/fecha_invalida/fecha_invalida'
+        )->assertStatus(Response::HTTP_NOT_FOUND);
     }
 
     public function test_crear_una_comision()
@@ -99,16 +103,19 @@ class ComisionModuleTest extends TestCase
         $this->withoutMiddleware();
         $this->withoutExceptionHandling();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->make([
-            'comisionNombre' => 'Comision de Prueba',
-            'comisionHorario' => 'Wednesday 02:33:54',
-            'comisionFI' => '2017-10-15',
-            'comisionFF' => '2018-07-15',
-            'cursoId' => 1
-            ])->toArray();
+        $comision = factory(Comision::class)
+            ->make([
+                'comisionNombre' => 'Comision de Prueba',
+                'comisionHorario' => 'Wednesday 02:33:54',
+                'comisionFI' => '2017-10-15',
+                'comisionFF' => '2018-07-15',
+                'cursoId' => 1,
+            ])
+            ->toArray();
 
-        $this->post('api/comisiones', $comision)
-            ->assertStatus(Response::HTTP_CREATED);
+        $this->post('api/comisiones', $comision)->assertStatus(
+            Response::HTTP_CREATED
+        );
         $this->assertCount(1, Comision::all());
     }
 
@@ -117,21 +124,42 @@ class ComisionModuleTest extends TestCase
         $this->assertTrue(true);
         return [
             'Requiere el campo Nombre' => ['comisionNombre', null],
-            'El campo Nombre tiene como maximo 150 caracteres' => ['comisionNombre', 'El campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteres'],
-            'El campo Nombre requiere como minimo 3 caracteres' => ['comisionNombre', 'ab'],
+            'El campo Nombre tiene como maximo 150 caracteres' => [
+                'comisionNombre',
+                'El campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteres',
+            ],
+            'El campo Nombre requiere como minimo 3 caracteres' => [
+                'comisionNombre',
+                'ab',
+            ],
 
             'Requiere el campo Horario' => ['comisionHorario', null],
-            'El campo Horario tiene como maximo 50 caracteres' => ['comisionHorario',  'El campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteres El campo Nombre tiene como maximo 150 caracteres'],
-            'El campo Horario requiere como minimo 3 caracteres' => ['comisionHorario', 'ab'],
+            'El campo Horario tiene como maximo 50 caracteres' => [
+                'comisionHorario',
+                'El campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteresEl campo Nombre tiene como maximo 150 caracteres El campo Nombre tiene como maximo 150 caracteres',
+            ],
+            'El campo Horario requiere como minimo 3 caracteres' => [
+                'comisionHorario',
+                'ab',
+            ],
 
             'Requiere el campo Fecha de Inicio' => ['comisionFI', null],
-            'El valor del campo Fecha de Inicio es incorrecto' => ['comisionFI', 'valor_invalido'],
+            'El valor del campo Fecha de Inicio es incorrecto' => [
+                'comisionFI',
+                'valor_invalido',
+            ],
 
             'Requiere el campo Fecha de Fin' => ['comisionFF', null],
-            'El valor del campo Fecha de Fin es incorrecto' => ['comisionFF', 'valor_invalido'],
+            'El valor del campo Fecha de Fin es incorrecto' => [
+                'comisionFF',
+                'valor_invalido',
+            ],
 
             'Requiere el campo Curso ' => ['cursoId', null],
-            'El valor del campo Curso es incorrecto' => ['cursoId', 'valor_invalido'],
+            'El valor del campo Curso es incorrecto' => [
+                'cursoId',
+                'valor_invalido',
+            ],
         ];
     }
 
@@ -141,23 +169,27 @@ class ComisionModuleTest extends TestCase
     public function test_validacion_request_al_crea_una_comision(
         string $campo,
         $valor
-    ){
+    ) {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
         $comision = factory(Comision::class)->make([$campo => $valor]);
-        $this->post('api/comisiones/',$comision->toArray())
+        $this->post('api/comisiones/', $comision->toArray())
             ->assertStatus(Response::HTTP_FOUND)
             ->assertSessionHasErrors($campo);
     }
-
 
     public function test_modificar_una_comision()
     {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->create()->toArray();
+        $comision = factory(Comision::class)
+            ->create()
+            ->toArray();
         $comision['comisionNombre'] = 'Nombre Modificado';
-        $this->put(sprintf('api/comisiones/%s', $comision['comisionId']), $comision)
+        $this->put(
+            sprintf('api/comisiones/%s', $comision['comisionId']),
+            $comision
+        )
             ->assertExactJson($comision)
             ->assertStatus(Response::HTTP_OK);
     }
@@ -168,12 +200,17 @@ class ComisionModuleTest extends TestCase
     public function test_validacion_request_al_modificar_una_comision(
         string $campo,
         $valor
-    ){
+    ) {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->create()->toArray();
+        $comision = factory(Comision::class)
+            ->create()
+            ->toArray();
         $comision[$campo] = $valor;
-        $this->put(sprintf('api/comisiones/%s', $comision['comisionId']), $comision)
+        $this->put(
+            sprintf('api/comisiones/%s', $comision['comisionId']),
+            $comision
+        )
             ->assertStatus(Response::HTTP_FOUND)
             ->assertSessionHasErrors($campo);
     }
@@ -182,25 +219,35 @@ class ComisionModuleTest extends TestCase
     {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->create()->toArray();
-        $this->delete('api/comisiones', $comision)
-            ->assertStatus(Response::HTTP_NO_CONTENT);
+        $comision = factory(Comision::class)->create();
+        // ->toArray();
+        $this->delete('api/comisiones', $comision->toArray())->assertStatus(
+            Response::HTTP_NO_CONTENT
+        );
         $this->assertCount(0, Comision::all());
+        $this->assertDeleted($comision);
     }
 
     public function test_eliminar_una_comision_con_alumnos_inscriptos()
     {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comision = factory(Comision::class)->create()->toArray();
-        $estudiante = factory(Estudiante::class)->create()->toArray();
-        factory(Matricula::class)->create([
-            'estudianteId' => $estudiante['estudianteId'],
-            'comisionId' => $comision['comisionId'],
-            'matriculaSituacion' =>  'RE'
-            ])->toArray();
-        $this->delete('api/comisiones', $comision)
-            ->assertStatus(Response::HTTP_FOUND);
+        $comision = factory(Comision::class)
+            ->create()
+            ->toArray();
+        $estudiante = factory(Estudiante::class)
+            ->create()
+            ->toArray();
+        factory(Matricula::class)
+            ->create([
+                'estudianteId' => $estudiante['estudianteId'],
+                'comisionId' => $comision['comisionId'],
+                'matriculaSituacion' => 'RE',
+            ])
+            ->toArray();
+        $this->delete('api/comisiones', $comision)->assertStatus(
+            Response::HTTP_FOUND
+        );
         $this->assertCount(1, Comision::all());
     }
 
@@ -208,16 +255,17 @@ class ComisionModuleTest extends TestCase
     {
         $this->withoutMiddleware();
         factory(Curso::class)->create();
-        $comisionNoExiste = factory(Comision::class)->make()->toArray();
-        $this->delete('api/comisiones', $comisionNoExiste)
-            ->assertStatus(Response::HTTP_FOUND);
+        $comisionNoExiste = factory(Comision::class)
+            ->make()
+            ->toArray();
+        $this->delete('api/comisiones', $comisionNoExiste)->assertStatus(
+            Response::HTTP_FOUND
+        );
     }
-
 
     /** afirmo que en la tabla estudiante se encuentra el email
      * $this->assertDatabaseHas('estudiantes', [
      *     'estudianteEmail' => $estudianteDatos['estudianteEmail'],
      *  ]);
      */
-
 }
