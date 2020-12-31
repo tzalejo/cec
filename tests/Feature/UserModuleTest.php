f52def3e6777f1ec56793695a09b2011d1b7d8a2
alejandro <tzalejo@gmail.com>
2020-12-31 02:33:42 -0300

    mod: bugs al generar el rol con factory

--- tests/Feature/UserModuleTest.php
+++ tests/Feature/UserModuleTest.php
@@ -7,12 +7,12 @@ use App\Role;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\Response;
 use Laravel\Passport\Passport;
 class UserModuleTest extends TestCase
 {
     use RefreshDatabase; # para actualiza la base con la base_test(realiza las migraciones)
-
     public function test_autenticacion_de_un_usuario()
     {
         /**
@@ -21,7 +21,9 @@ class UserModuleTest extends TestCase
          */
         Artisan::call('passport:install');
         factory(Role::class)->create();
-        $user = factory(User::class)->create()->toArray();
+        $user = factory(User::class)
+            ->create()
+            ->toArray();
         // dd($user);
         $credentials = [
             'email' => $user['email'],
@@ -29,13 +31,13 @@ class UserModuleTest extends TestCase
         ];
         $this->post('api/auth/login', $credentials)
             ->assertJsonStructure([
-                'userNombre' ,
-                'email'      ,
-                'userImagen' ,
-                'roleId'     ,
-                'token'      ,
-                'token_type' ,
-                'expires_at' ,
+                'userNombre',
+                'email',
+                'userImagen',
+                'roleId',
+                'token',
+                'token_type',
+                'expires_at',
             ])
             ->assertStatus(Response::HTTP_OK);
     }
@@ -46,8 +48,9 @@ class UserModuleTest extends TestCase
             'email' => 'correo_no_registrado@email.com',
             'password' => 'secret',
         ];
-        $this->post('api/auth/login', $credentials)
-            ->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->post('api/auth/login', $credentials)->assertStatus(
+            Response::HTTP_UNAUTHORIZED
+        );
     }
 
     public function test_validacion_request_de_autenticacion()
@@ -56,17 +59,20 @@ class UserModuleTest extends TestCase
         return [
             'El mail es requerido para la autenticacion' => ['email', null],
             'No es un mail valido' => ['email', 'email_no_valido'],
-            'El password es requerido para la autenticacion' => ['password', null],
+            'El password es requerido para la autenticacion' => [
+                'password',
+                null,
+            ],
         ];
     }
 
     /**
      * @dataProvider test_validacion_request_de_autenticacion
      */
-    public function test_valida_los_datos_de_autenticacion($campo,$valor)
+    public function test_valida_los_datos_de_autenticacion($campo, $valor)
     {
         $credentials = [
-            'email' =>  'test@email.com',
+            'email' => 'test@email.com',
             'password' => 'test',
         ];
         $credentials[$campo] = $valor;
@@ -79,7 +85,9 @@ class UserModuleTest extends TestCase
     {
         // $this->withoutMiddleware();
         // $this->withoutExceptionHandling();
-        factory(Role::class)->create();
+        factory(Role::class)->create([
+            'roleDescripcion' => Config::get('constants.USER'),
+        ]);
         $user = [
             'userNombre' => 'eliseo nicolas',
             'email' => 'myriam.crona@example.net',
@@ -89,29 +97,37 @@ class UserModuleTest extends TestCase
         ];
         $this->post('api/auth/signup', $user)
             ->assertJsonStructure(['message'])
-            ->assertExactJson(['message' => 'Usuario Eliseo Nicolas creado satisfactoriamente'])
+            ->assertExactJson([
+                'message' => 'Usuario Eliseo Nicolas creado satisfactoriamente',
+            ])
             ->assertStatus(Response::HTTP_CREATED);
         $this->assertCount(1, User::all());
     }
-
 
     public function test_valida_request_registro()
     {
         $this->assertTrue(true);
         return [
-            'El Nombre del Usuario es requerido para la autenticacion' => ['userNombre', null],
+            'El Nombre del Usuario es requerido para la autenticacion' => [
+                'userNombre',
+                null,
+            ],
             'El mail es requerido para la autenticacion' => ['email', null],
-            'El password es requerido para la autenticacion' => ['password', null],
-            'No es un mail valido' => ['email', 'email_no_valido']
+            'El password es requerido para la autenticacion' => [
+                'password',
+                null,
+            ],
+            'No es un mail valido' => ['email', 'email_no_valido'],
         ];
     }
-
 
     /**
      * @dataProvider test_valida_request_registro
      */
-    public function test_valida_los_datos_del_registro_de_un_usuario($campo, $valor)
-    {
+    public function test_valida_los_datos_del_registro_de_un_usuario(
+        $campo,
+        $valor
+    ) {
         factory(Role::class)->create();
         $user = [
             'userNombre' => 'eliseo nicolas',
@@ -120,7 +136,7 @@ class UserModuleTest extends TestCase
             'userImagen' => 'test/test.png',
             'roleId' => 1,
         ];
-        $user[$campo]= $valor;
+        $user[$campo] = $valor;
         // dd($user);
         $this->post('api/auth/signup', $user)
             ->assertSessionHasErrors($campo)
@@ -131,14 +147,13 @@ class UserModuleTest extends TestCase
     public function test_registro_de_un_usuario_con_un_mail_ya_registrado()
     {
         factory(Role::class)->create();
-        $user = factory(User::class)->create()->toArray();
+        $user = factory(User::class)
+            ->create()
+            ->toArray();
         // dd($user);
         $this->post('api/auth/signup', $user)
             ->assertSessionHasErrors(['email'])
             ->assertStatus(Response::HTTP_FOUND);
         $this->assertCount(1, User::all());
     }
-
-    
-
 }
